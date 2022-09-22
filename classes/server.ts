91aeb705a5b72bc6d6d7be170276b95c5fef0b81
zule67 <zule67@hotmail.com>
2022-09-22 18:16:32 +0200

    Fin sección 5

--- classes/server.ts
+++ classes/server.ts
@@ -33,13 +33,19 @@ export default class Server {
         console.log('Escuchando conexiones');
 
         this.io.on('connection', cliente => {
-            console.log('Cliente conectado');
 
+            // Conectar cliente
+            socket.conectarCliente(cliente);
+            
+            // Configurar Usuario
+            socket.configurarUsuario( cliente, this.io );
+            
             // Mensajes
             socket.mensaje( cliente, this.io );
 
             // Desconectar
-            socket.desconectar(cliente);
+            socket.desconectar( cliente );
+
         });
     }
 
--- routes/router.ts
+++ routes/router.ts
@@ -1,4 +1,5 @@
 import { Router, Request, Response } from 'express';
+import Server from '../classes/server';
 
 const router = Router();
 
@@ -14,6 +15,15 @@ router.post('/mensajes', (req: Request, res: Response)=> {
     const cuerpo = req.body.cuerpo;
     const de = req.body.de;
 
+    const payload = {
+        de,
+        cuerpo
+    };
+
+    const server = Server.instance;
+
+    server.io.emit( 'mensaje-nuevo', payload );
+
     res.json({
         ok: true,
         cuerpo,
@@ -26,6 +36,15 @@ router.post('/mensajes/:id', (req: Request, res: Response)=> {
     const cuerpo = req.body.cuerpo;
     const de = req.body.de;
     const id = req.params.id;
+
+    const payload = {
+        de,
+        cuerpo
+    };
+
+    const server = Server.instance;
+
+    server.io.in( id ).emit( 'mensaje-privado', payload );
 
     res.json({
         ok: true,
